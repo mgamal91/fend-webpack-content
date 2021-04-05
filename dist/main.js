51796b65bafa96c725ad96edc5869cc981dbb278
MGamal <miada.gamal@gmail.com>
2021-04-05 23:33:43 +0200

    babel_done

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,1 @@
-alert("hi memo");
+(()=>{"use strict";console.log((function(e){console.log("::: Running checkForName :::",e),["Picard","Janeway","Kirk","Archer","Georgiou"].includes(e)&&alert("Welcome, Captain!")})),alert("I EXIST")})();
