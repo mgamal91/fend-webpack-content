e09e1134157c38365217d9a0c510a8d224ddfc64
MGamal <miada.gamal@gmail.com>
2021-04-06 03:00:29 +0200

    file-loader

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,7 @@
 /* alert("hi memo"); */
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
-
+import img from './image.jpg';
 console.log(checkForName);
 
 alert("I EXIST")
--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,15 @@ module.exports = {
                     test: '/\.js$/',
                     exclude: /node_modules/,
                     loader: "babel-loader"
+                },
+                {
+                    test: /\.(png|jpe?g|gif)$/i,
+                    use: 
+                    [
+                        {
+                            loader: 'file-loader',
+                        }
+                    ]
                 }
         ]
     }
