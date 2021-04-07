86a088ebff882b2b0aab2444ab375b67afac6bf6
MGamal <miada.gamal@gmail.com>
2021-04-07 15:14:17 +0200

    plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path")
 const webpack = require("webpack")
+const HtmlWebpackPlugin=require("html-webpack-plugin");
 module.exports = {
     entry: './src/client/index.js',
     module: {
@@ -10,5 +11,16 @@ module.exports = {
                     loader: "babel-loader"
                 }
         ]
-    }
+    },
+    plugins:
+    [
+        new HtmlWebpackPlugin
+        (
+            {
+                template:"./src/client/views/index.html",
+                filename:"./index.html"
+
+            }
+        )
+    ]
 }
