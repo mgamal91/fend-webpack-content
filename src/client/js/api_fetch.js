e8a824eda52444494ce7aaeee22490c765b4f09b
MGamal <miada.gamal@gmail.com>
2021-04-10 03:49:52 +0200

    memasdsa

--- src/client/js/api_fetch.js
+++ src/client/js/api_fetch.js
@@ -3,7 +3,7 @@ export function fetchAPI(event) {
 
     // check what text was put into the form field
     let formText = document.getElementById('memo').value;
-    let test2=document.getElementById('resultsAPI').value;
+    let test2=document.getElementById('resultsAPI');
     let p=document.createElement('p');
     let url='http://api.openweathermap.org/data/2.5/weather?q=London&appid=e52537ee7c988a3d6e76f5cf45bd0583';
     console.log("::: Fetch API :::");
@@ -16,19 +16,3 @@ export function fetchAPI(event) {
         test2.innerHTML = res.message
     })
 }
-
-    /* fetch(url)
-.then((resp) => resp.json())
-.then(function(data) 
-{
-  let authors = data.results;
-  /* return authors.map(function(author) { */
-    test2.innerHTML = `${author.main.first} ${author.name.last}`;
-    
-    append(li, span);
-    append(ul, li);
- /*  } )*/
-/*} ) */
-/* .catch(function(error) {
-  console.log(error);
-}); */
