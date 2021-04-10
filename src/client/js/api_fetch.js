4d8bb05076551e203fd486df7bb9e69e5a987c5b
MGamal <miada.gamal@gmail.com>
2021-04-10 04:15:06 +0200

    sada

--- src/client/js/api_fetch.js
+++ src/client/js/api_fetch.js
@@ -3,8 +3,9 @@ export function fetchAPI(event) {
 
     // check what text was put into the form field
     let formText = document.getElementById('memo').value;
+    //the result will be put here
     let test2=document.getElementById('resultsAPI');
-    let p=document.createElement('p');
+    //url of the api we are going to fetch
     let url='http://api.openweathermap.org/data/2.5/weather?q=London&appid=e52537ee7c988a3d6e76f5cf45bd0583';
     console.log("::: Fetch API :::");
     console.log(url);
@@ -13,6 +14,7 @@ export function fetchAPI(event) {
     
     .then(res => res.json())
     .then(function(res) {
+        //i am testing with 2 outputs from my APi, one is base and the other is the temp which is inside main(main.temp)
         console.log(res.base);
         test2.innerHTML = res.main.temp;
     })
