9e2068078468d941d31ec6aebb84b1b0f56cdc51
MGamal <miada.gamal@gmail.com>
2021-04-10 05:56:48 +0200

    adsa

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,12 @@
 const path = require("path")
 const webpack = require("webpack")
-const HtmlWebpackPlugin=require("html-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 module.exports = {
     mode: 'production',
+    devtool: 'source-map',
     entry: './src/client/index.js',
     output: {
         libraryTarget: 'var',
@@ -13,30 +14,25 @@ module.exports = {
     },
     optimization: {
         minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
-        },
+    },
     module: {
-        rules: [
-                {
-                    test: '/\.js$/',
-                    exclude: /node_modules/,
-                    loader: "babel-loader"
-                },
-                {
-                    test: /\.scss$/,
-                   use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-                }
+        rules: [{
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+            }
         ]
     },
-    plugins:
-    [
-        new HtmlWebpackPlugin
-        (
-            {
-                template:"./src/client/views/index.html",
-                filename:"./index.html"
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html"
 
-            }
-        ),
+        }),
         new MiniCssExtractPlugin({ filename: "[name].css" })
     ]
 }
