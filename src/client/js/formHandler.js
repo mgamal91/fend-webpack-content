57d8ff9303db4c7a55fa6700afba2881fe90b53f
MGamal <miada.gamal@gmail.com>
2021-04-10 03:18:02 +0200

    a

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -6,7 +6,9 @@ export function handleSubmit(event) {
     Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
+    //fetch('http://localhost:8081/test')
+    fetch('api.openweathermap.org/data/2.5/weather?q=London&appid=e52537ee7c988a3d6e76f5cf45bd0583')
+    
     .then(res => res.json())
     .then(function(res) {
         document.getElementById('results').innerHTML = res.message
