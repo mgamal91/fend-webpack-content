8a0c51d2bd594767de475caa50a196f65878e16e
MGamal <miada.gamal@gmail.com>
2021-04-17 20:03:37 +0200

    api+service

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,7 @@
 /* alert("hi memo"); */
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
+import { fetchAPI } from './js/api_fetch'
 import './styles/resets.scss'
 import './styles/base.scss'
 import './styles/footer.scss'
---
+++ src/client/js/api_fetch.js
@@ -0,0 +1,21 @@
+export function fetchAPI(event) {
+    event.preventDefault()
+
+    // check what text was put into the form field
+    let formText = document.getElementById('memo').value;
+    //the result will be put here
+    let test2 = document.getElementById('resultsAPI');
+    //url of the api we are going to fetch
+    let url = 'http://api.openweathermap.org/data/2.5/weather?q=London&appid=e52537ee7c988a3d6e76f5cf45bd0583';
+    console.log("::: Fetch API :::");
+    console.log(url);
+    //fetch('http://localhost:8081/test')
+    fetch(url)
+
+    .then(res => res.json())
+        .then(function(res) {
+            //i am testing with 2 outputs from my APi, one is base and the other is the temp which is inside main(main.temp)
+            console.log(res.base);
+            test2.innerHTML = res.main.temp;
+        })
+}
